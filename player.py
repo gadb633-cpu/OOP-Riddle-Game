86c3f5a440150f56b7ffaaf462423fabadf5f860
gad-biton <gadb633@gmail.com>
2026-08-18 17:21:58 +0300

    fixed username

--- player.py
+++ player.py
@@ -2,8 +2,8 @@ class Player():
     def __init__(self,username):
         self.__username = username
     @property
-    def username(self) -> str:
+    def get_username(self) -> str:
         return self.__username
-    
     def rename(self, new_username: str) -> None:
         self.__username = new_username
+username = Player(input("enter usernsme: "))        
