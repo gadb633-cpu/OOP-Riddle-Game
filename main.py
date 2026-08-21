8044f597d7ceeead5e8c8d0141c9c33c99c9ca59
gad-biton <gadb633@gmail.com>
2026-08-21 17:11:16 +0300

    added update_riddle

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from riddle import *
 from player import *
 from repository import *
 riddles_list = RiddleRepository("riddles.json")
-# riddles_list.save_riddles(riddles_list.add_riddle())
-# print(riddles_list.get_all_riddles())
-riddles_list.update_riddle()
+riddles_list.save_riddles(riddles_list.add_riddle())
+print(riddles_list.get_all_riddles())
+riddles_list.save_riddles(riddles_list.update_riddle(riddles_list.get_all_riddles()))
 manager = RiddleGame(username.get_username,riddles_list.load_riddles())
 manager.print_summary(manager.start())
--- repository.py
+++ repository.py
@@ -1,23 +1,26 @@
 from json import *
 from riddle import *
+import os
+import json
+from json import JSONDecodeError
 class RiddleRepository:
     def __init__(self,file_path):
         self.__file_path = file_path
     def load_riddles(self) -> list[Riddle]:    
-        with open("riddles.json", "r", encoding="utf-8") as file:
+        with open("riddles.json", "r", encoding="utf-8-sig") as file:
             data = load(file)
-        Riddles= []
+        riddles= []
         for i in data:
             if i["type"] == "multiple_4":
                 instance1 = FourAnswerRiddle(i["id"],i["question"],i["correct_answer"],i["difficulty"],i["category"],i["possible_answers"])
-                Riddles.append(instance1)
+                riddles.append(instance1)
             elif i["type"] == "multiple_2":
                 instance2 = TwoAnswerRiddle(i["id"],i["question"],i["correct_answer"],i["difficulty"],i["category"],i["possible_answers"])
-                Riddles.append(instance2)
+                riddles.append(instance2)
             elif i["type"] == "open":
                 instance3 = OpenRiddle(i["id"],i["question"],i["correct_answer"],i["difficulty"],i["category"])
-                Riddles.append(instance3)
-        return Riddles 
+                riddles.append(instance3)
+        return riddles 
     def add_riddle(self) -> None:
         id_ = int(input("enter id: "))
         question = input("enter question: ")
@@ -49,10 +52,34 @@ class RiddleRepository:
         riddles =load(f)
         f.close()
         return riddles
-    def update_riddle(self, riddle_id: int, new_data: dict) -> bool:
-        riddle_id
+    def update_riddle(self,riddles) -> bool:
+        new_riddles = []
+        riddle_id = int(input("enter id: "))
+        status = True
+        while status:
+            new_key_to_change = input("Enter the key: ")
+            new_value_to_change = input("Enter the value: ")
+            for riddle in riddles:
+                if riddle_id == riddle["id"]:
+                    if new_key_to_change in riddle:
+                        riddle[new_key_to_change] = new_value_to_change
+                        new_riddles.append(riddle)
+                        print(riddle)      
+                    else:
+                        # print("the key is not excist! ")
+                        new_riddles.append(riddle)
+                        continue    
+                else:
+                    # print("the id is not excist! ")
+                    new_riddles.append(riddle)
+                    continue
+            x = input("you want to update more? ")
+            if x == "no":
+                status = False
+        return new_riddles     
     def delete_riddle(self, riddle_id: int) -> bool:
         pass   
     def save_riddles(self, riddles: list) -> None:
         with open(self.__file_path, "w") as file:
             dump(riddles, file,indent=4)
+           
